2e5a08b1fbbc9c2ce7bd783df9d4aed6349474cd
jaden <18636621167@163.com>
2018-12-08 11:22:32 +0800

    apigateway

---
+++ src/main/java/qy/springclout/apigateway/ZuulApiGatewayApplication.java
@@ -0,0 +1,28 @@
+package qy.springclout.apigateway;
+
+import com.netflix.hystrix.HystrixCommandProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @Author: Jaden
+ * @Description:
+ * @Date: Created in 10:43 2018/12/8
+ * @Modified By:
+ */
+
+@EnableZuulProxy
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ZuulApiGatewayApplication {
+    public static void main(String[] args) {
+        HystrixCommandProperties.Setter()
+                .withExecutionTimeoutEnabled(false);
+        HystrixCommandProperties.Setter()
+                .withExecutionTimeoutInMilliseconds(61000);
+        SpringApplication.run(ZuulApiGatewayApplication.class, args);
+    }
+
+}
